2cf6b1ba160f8c79aab5edf57e8ee755ea404663
Ronasunil <ronasunilcoc@gmail.com>
2024-11-07 16:17:43 +0530

    Home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.scss";
+function Home() {
+  return (
+    <>
+      <div>Header</div>
+      <div className="dashboard">
+        <div className="dashboard-sidebar">sidebar</div>
+        <div className="dashboard-content">content</div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
